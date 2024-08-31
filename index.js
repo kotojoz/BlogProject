493b41fe385649434e4d00e74e48d4db9fdbc543
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2024-08-31 12:21:03 +0300

    Add basic Express server and initial EJS views

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 
+const app = express();
+const port = 3000;
+
+let allPosts = [new Post(1, "First", "I`m tripping"), new Post(2, "Second", "I`m tired")];
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {posts: allPosts});
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+})
+
+function Post(id, title, content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+}
